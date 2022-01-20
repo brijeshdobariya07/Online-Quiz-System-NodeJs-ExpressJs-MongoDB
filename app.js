ef2e91f94396de97d66985fa911f5f8dcf54d854
Brijesh <48529265+brijeshdobariya07@users.noreply.github.com>
2022-01-20 16:13:47 +0530

    added js extension in conn file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs'); 
-require('./src/db/conn');
+require('./src/db/conn.js');
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
